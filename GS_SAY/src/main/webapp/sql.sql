72c5c6625e9909fc9b2ac513dea37e63a723afa6
bigdata02 <bigdata02@59.0.147.243>
2021-08-03 18:02:58 +0900

    sql 수정

--- GS_SAY/src/main/webapp/sql.sql
+++ GS_SAY/src/main/webapp/sql.sql
@@ -7,7 +7,7 @@ drop table endinfo;
 create table memberlist (
 member_id varchar2(15),
 member_pw varchar2(15) not null,
-member_name varchar2(10) not null,
+member_name varchar2(15) not null,
 member_tel varchar2(15) not null,
 constraint memberlist_id_pk primary key(member_id));
 
@@ -15,7 +15,7 @@ select * from memberlist;
 
 create table maininfo (
 info_num number(10),
-info_name varchar2(50) not null,
+info_name varchar2(150) not null,
 start_day date,
 end_day date,
 info_link varchar2(500) not null,
@@ -36,9 +36,9 @@ scrap_num number(10),
 member_id varchar2(15),
 info_num number(20),
 end_day date,
-info_name varchar2(50) not null,
+info_name varchar2(150) not null,
 info_link varchar2(500) not null,
-constraint myscrap_scrap_num_pk primary key(scrap_num),
+constraint myscrap_scrap_num_pk primary key (scrap_num),
 constraint myscrap_member_id_fk foreign key (member_id) references memberlist(member_id),
 constraint myscrap_info_num_fk foreign key (info_num) references maininfo(info_num))
 
@@ -47,10 +47,10 @@ start with 1 increment by 1
 
 create table endinfo(
 info_num number(10),
-info_name varchar2(50) not null,
+info_name varchar2(150) not null,
 start_day date,
 end_day date,
 info_link varchar2(500) not null,
-constraint endinfo_info_num_pk primary key(info_num),
+constraint endinfo_info_num_pk primary key (info_num),
 constraint endinfo_info_num_fk foreign key (info_num) references maininfo(info_num))
 

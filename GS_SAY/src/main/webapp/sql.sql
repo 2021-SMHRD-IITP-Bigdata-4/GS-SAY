6ccd1b5a480a64862ad3f7a9e666ec6d3883244e
ts-11 <ts-11@DESKTOP-73L9EDP>
2021-08-04 18:30:27 +0900

    sql수정 및 category sql 수정

--- GS_SAY/src/main/webapp/sql.sql
+++ GS_SAY/src/main/webapp/sql.sql
@@ -19,7 +19,7 @@ constraint memberlist_id_pk primary key(member_id));
 
 create table category(
 code number(10),
-cate_name varchar2(20) not null,
+cate_name varchar2(50) not null,
 constraint category_code_pk primary key(code));
 
 create table maininfo (
@@ -42,7 +42,7 @@ info_name varchar2(150) not null,
 info_link varchar2(500) not null,
 constraint myscrap_scrap_num_pk primary key (scrap_num),
 constraint myscrap_member_id_fk foreign key (member_id) references memberlist(member_id),
-constraint myscrap_info_num_fk foreign key (info_num) references maininfo(info_num))
+constraint myscrap_info_num_fk foreign key (info_num) references maininfo(info_num));
 
 create table endinfo(
 endinfo_num number(10),
@@ -53,7 +53,7 @@ end_day date,
 code number(10) not null,
 info_link varchar2(500) not null,
 constraint endinfo_endinfo_num_pk primary key (endinfo_num),
-constraint endinfo_info_num_fk foreign key (info_num) references maininfo(info_num))
+constraint endinfo_info_num_fk foreign key (info_num) references maininfo(info_num));
 
 create sequence info_num
 start with 1 increment by 1
